560d01ce34f1b1a9c8bf1fa54a577e0135373822
yunyou <502765299@qq.com>
2026-01-04 23:23:57 +0800

    Initial Commit

---
+++ learnopenglagain/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learnopenglagain
+//
+//  Created by 苗云龙 on 2026/1/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
